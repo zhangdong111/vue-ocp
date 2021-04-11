dab9e74629bff68f664e22d52a9715a44fc05014
zhangdong <15101575120@sina.cn>
2021-04-11 22:29:03 +0800

    插槽

---
+++ src/components/Father.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>1</div>
+    <slot v-solt="buttonStyle">
+      <el-radio>2</el-radio>
+    </slot>
+    <div>3</div>
+  </div>
+</template>
+
+<script>
+export default({
+    
+    setup() {
+        
+    },
+})
+</script>
+
---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Father></Father>
+    <Father><solt name="buttonStyle"><el-button>11</el-button></solt></Father>
+  </div>
+</template>
+
+<script>
+import Father from '@/components/Father.vue'
+export default({
+  components :{
+    Father
+  },
+  setup() {
+    
+  }
+})
+</script>
+
+
